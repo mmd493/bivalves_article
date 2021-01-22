dc0362801700054985c6196aa8214c46ba866e60
Megan Davis <megandavis@Megans-MBP.home>
2021-01-22 06:54:21 -0800

    Update to R script and add bivalves and production csv.

---
+++ bivalves_analysis.R
@@ -0,0 +1,48 @@
+### MARINE BIVALVE AQUCULTURE MASTER SCRIPT
+### Megan Davis
+### In collaboration with Dr. Sonali McDermid and Dr. Jennifer Jacquet
+
+##### BELOW NEEDS TO BE CLEANED #####
+
+##Insert explanation of project...
+
+#----------------#
+##### SET UP #####
+#----------------#
+
+##Prepare the workspace to run the analysis.
+
+##Clear workspace.
+rm(list = ls())
+
+##Load libraries.
+library(dplyr)
+library(stringr)
+
+##Set the working directory.
+setwd("/Users/MeganDavis/Documents/r_code/bivalves_article")
+
+aqu_prod <- read.csv(file = "data/fao_aquaculture_prod.csv", header = TRUE, sep = ",") %>%
+  select("country" = Land.Area, "species" = Species, "scientific_name" = Scientific.name, "production_2018" = X2018)
+
+bivalves <- aqu_prod %>%
+  distinct(species, scientific_name) %>%
+  mutate(species = case_when((species %in% "..A") == T ~"Pullet carpet shell",
+                             (species %in% "..A") == F ~ species))
+
+write.csv(bivalves, "/Users/MeganDavis/Documents/r_code/bivalves_article/data/bivalves.csv")
+
+library(rfishbase)
+
+species_list = c("Mytilus galloprovincialis")
+
+species(species_list, fields="Comments")
+
+
+------------------
+
+##Notes on general layout
+
+#Keep this script on the shorter end. It will be the script that runs everything else. But for now it will also act as your
+#workspace.
+
